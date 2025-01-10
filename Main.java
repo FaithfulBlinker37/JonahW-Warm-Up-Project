23b6fe3c661358f6ebc6a658d15a3a09a385519b
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-01-10 22:39:20 +0000

    commit#

--- Main.java
+++ Main.java
@@ -2,9 +2,3 @@ public class TotalSum {
     public static void main(String[] args) {
         System.out.println(totalSum(2000000));
     }
-
-    public static long totalSum(int limit) {
-        boolean[] isPrime = new boolean[limit];
-        for (int i = 2; i < limit; i++) {
-            isPrime[i] = true;
-        }
